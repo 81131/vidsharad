7a92dc6e8187b09b3697639fe7f203b598651c3d
81131 <saluwadanagedaradinindu@gmail.com>
2026-09-09 16:20:33 +0530

    styles: removed the generic header element

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
 import UploadForm from "./components/UploadForm";
 
 function App() {
-  return (
-    <div>
-      <h1>Video Converter</h1>
-      <UploadForm />
-    </div>
-  );
+  return <UploadForm />;
 }
 
 export default App;
