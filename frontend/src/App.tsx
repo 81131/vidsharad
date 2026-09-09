7495c75d62285d2d865418b9b17b86608a04ac4e
81131 <saluwadanagedaradinindu@gmail.com>
2026-09-09 14:38:24 +0530

    feat: added UploadForm component to the App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import UploadForm from "./components/UploadForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Video Converter</h1>
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
